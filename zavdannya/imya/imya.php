3637555e154757a4f37a22b6c99790e47b111a3a
Lina Sayko <galinakkerman@gmail.com>
2019-09-27 20:22:03 +0300

    comm

---
+++ zavdannya/imya/imya.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Hello</title>
+	<meta charset="utf-8">
+</head>
+<body>
+	<?php
+	$imya = $_POST["imya"];
+	echo "<span>Привіт, ".$imya." </span>";
+	?>
+</body>
+</html>
